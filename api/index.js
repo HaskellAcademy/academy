226d65f15fc99fd48ac4dda2582bcb7820e70cf0
Sunjay Varma <varma.sunjay@gmail.com>
2016-10-22 01:55:28 -0400

    Added response time middleware

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const koa = require('koa');
 const logger = require('koa-logger');
 const limit = require('koa-better-ratelimit');
 const compress = require('koa-compress');
+const responseTime = require('koa-response-time');
 
 const app = koa();
+
+app.use(responseTime());
 
 app.use(limit({
   duration: 3*60*1000,
