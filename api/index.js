06fccbfb099a1f395a258dfa24d01967977a9c6b
Sunjay Varma <varma.sunjay@gmail.com>
2016-10-22 01:43:21 -0400

    Added rate limiting to the API

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const koa = require('koa');
 const logger = require('koa-logger');
+const limit = require('koa-better-ratelimit');
 
 const app = koa();
+
+app.use(limit({
+  duration: 3*60*1000,
+  max: 100,
+  blacklist: ['127.0.0.1'],
+}));
 
 app.use(logger());
 
