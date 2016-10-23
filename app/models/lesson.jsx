232451b19e14b062413745ffd213733e8bebfa6f
Sunjay Varma <varma.sunjay@gmail.com>
2016-10-23 00:35:41 -0400

    Added status field to lesson model

--- app/models/lesson.jsx
+++ app/models/lesson.jsx
@@ -1,10 +1,13 @@
 const {createRecord} = require('./model');
 
 const LessonRecord = createRecord({
-}, () => ({
+  STATUS_PUBLISHED: 'published',
+  STATUS_DISABLED: 'disabled',
+}, (constants) => ({
   id: undefined,
   title: undefined,
   summary: undefined,
+  status: constants.STATUS_PUBLISHED,
   body: undefined,
   instructions: undefined,
   // [{name: 'filename', content: 'file contents'}]
