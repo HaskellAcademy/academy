1f5dbdf88df5b01f8749c9e5da3349c97a8bb0bb
Sunjay Varma <varma.sunjay@gmail.com>
2016-11-14 00:21:21 -0500

    Login links on home page

--- app/components/pages/Home.jsx
+++ app/components/pages/Home.jsx
@@ -4,6 +4,19 @@ const {Link} = require('react-router');
 const Home = () => (
   <div>
     <h1>Hello, World!</h1>
+    <p>
+      <a className='btn btn-primary' href='http://api.local.haskellacademy.com:3000/auth/github'>
+        Login with GitHub
+      </a>
+      &nbsp;
+      <a className='btn btn-primary' href='http://api.local.haskellacademy.com:3000/auth/google'>
+        Login with Google
+      </a>
+      &nbsp;
+      <a className='btn btn-primary' href='http://api.local.haskellacademy.com:3000/auth/twitter'>
+        Login with Twitter
+      </a>
+    </p>
     <Link to='/lesson/1'>Lesson</Link>
   </div>
 );
